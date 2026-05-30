fb9a9a5d3f3bb5cc0ac49d9f87708ac2a2117d55
k4yr2 <kyrzkaya@gmail.com>
2026-05-30 03:52:06 +0300

    add Header component and navigation data; update layout to include Header

---
+++ src/data/navigation.ts
@@ -0,0 +1,14 @@
+export const navigation = [
+  {
+    href: "/ilan-ver",
+    label: "İş Ver",
+  },
+  {
+    href: "/is-bul",
+    label: "İş Bul",
+  },
+  {
+    href: "/nasil-calisir",
+    label: "Nasıl Çalışır",
+  },
+];
